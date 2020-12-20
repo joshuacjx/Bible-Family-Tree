25620729f5fcbb3ec3aa2b9684329763dd0dffa6
JoshuaChewJianXiang <joshuachewabc@gmail.com>
2020-12-20 15:32:24 +0800

    Allow Person to store parental and child data

--- family.py
+++ family.py
@@ -5,12 +5,36 @@ class Person:
     def __init__(self, id, name):
         self.id = id
         self.name = name
+        self.children = set()
+        self.mother = None
+        self.father = None
 
     def get_id(self):
         return self.id
 
     def get_name(self):
         return self.name
+
+    def add_child(self, child):
+        self.children.add(child)
+
+    def set_mother(self, mother):
+        self.mother = mother
+
+    def set_father(self, father):
+        self.father = father
+
+
+class Marriage:
+    def __init__(self, husband, wife):
+        self.husband = husband
+        self.wife = wife
+
+    def get_husband(self):
+        return self.husband
+
+    def get_wife(self):
+        return self.wife
 
 
 PERSON_FILEPATH = "person.csv"
@@ -23,14 +47,19 @@ relationship_data = pd.read_csv(RELATIONSHIP_FILEPATH)
 puml_text = ""
 
 # Collect all person data
-persons = []
+persons = dict()
 for index, row in person_data.iterrows():
-    person = Person(str(row['person_id']).replace(" ", "_"), str(row['person_name']))
-    persons.append(person)
+    name = str(row['person_name'])
+    id = str(row['person_id']).replace(" ", "_")
+    person = Person(id, name)
+    persons[id] = person
 
 # Declare all persons
-for person in persons:
+for person in persons.values():
     puml_text += "class \"" + person.get_name() + "\" as " + person.get_id() + "\n"
+
+# Collect all parental data
+
 
 print(puml_text)
 
